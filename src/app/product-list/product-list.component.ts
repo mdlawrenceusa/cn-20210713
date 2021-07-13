f3a02f46491612daa75cf8e56522fd4372f853bb
mdlawrenceusa <mdlawrence@me.com>
2021-07-13 12:17:24 -0400

    3 files modified, 3 added

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -13,6 +13,11 @@ export class ProductListComponent {
   share() {
     window.alert('The Phone has been shared!');
   }
+
+  onNotify() {
+    window.alert('You will be notified when the product goes on sale');
+  }
+
 }
 
 
